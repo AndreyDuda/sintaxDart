6cad4fbe6d307065e2a2c9cf85c22512229627bf
AndreyDuda <andreyduda88@gmail.com>
2021-10-20 02:44:09 +0300

    first class

--- bin/untitled.dart
+++ bin/untitled.dart
@@ -1,5 +1,29 @@
 import 'package:untitled/untitled.dart' as calculate;
 
+var num = 10;
+var word = 'Hello';
+
 void main(List<String> arguments) {
-  print('Hello world! ${calculate.calculate()} !!!');
+
+  var point;
+  point = 2;
+  point++;
+
+  point = 'sdf';
+
+  print('Hello world! ${point} !!!');
+
+  User user = User();
+  user.name = 'Bob';
+
+  print(user.name);
+}
+
+class User {
+  late String name;
+  late int age;
+  late bool isHappy;
+  late List<String> hobbies;
+
+  User();
 }
